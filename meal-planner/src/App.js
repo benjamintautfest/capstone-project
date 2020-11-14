6c8fc31eb321654104a3bd23f2cf171e8f6e79eb
Benjamin Tautfest <b.tautfest@gmx.de>
2020-11-14 11:54:09 +0100

    basic Setup

--- meal-planner/src/App.js
+++ meal-planner/src/App.js
@@ -1,15 +1,18 @@
 import styled from 'styled-components'
 
 export default function App() {
-  return (
-    <StyledDiv className="App">
-     <h1>Hello world!</h1>
-    </StyledDiv>
-  );
+    return (
+        <StyledDiv className="App">
+            <h1>Hello world!</h1>
+            <p>This is the initial setup for my Capstone project.</p>
+        </StyledDiv>
+    )
 }
 
 const StyledDiv = styled.div`
-font-family: sans-serif;
-text-align: center;
-color: yellowgreen;
+    font-family: sans-serif;
+    text-align: center;
+    h1 {
+        color: yellowgreen;
+    }
 `
