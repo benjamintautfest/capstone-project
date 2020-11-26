4257bcbbddff90df5aa63b8ce0f2f17b9d91cd8a
Benjamin Tautfest <b.tautfest@gmx.de>
2020-11-26 14:57:08 +0100

    Install react router

--- meal-planner/src/App.js
+++ meal-planner/src/App.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components/macro'
-import woodBackground from './assets/wood_background_knife.jpg'
-import logo from './assets/logo_2.svg'
+import woodBackground from './assets/wood_background.jpg'
+import logo from './assets/logo.svg'
 import Button from './components/Button'
 import MealSelectMenu from './components/MealSelectMenu'
 import RecipePage from './components/RecipePage'
@@ -35,7 +35,7 @@ export default function App() {
                 ) : (
                     ''
                 )}
-                {weekdays.length > 0 &&
+                {weekdays &&
                     weekdays.map(({ weekday, id, meal }) => (
                         <div key={id}>
                             <Button
