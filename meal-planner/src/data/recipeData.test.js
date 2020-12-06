23c8f6a5e4869505abd4db283d04b219b264ce90
Benjamin Tautfest <b.tautfest@gmx.de>
2020-12-06 10:56:17 +0100

    Add tests to json files

--- meal-planner/src/data/recipeData.test.js
+++ meal-planner/src/data/recipeData.test.js
@@ -1,6 +1,6 @@
 import recipeData from './recipeData.json'
 
-test('the recipe data is correct', () => {
+it('delivers the correct recipe data', () => {
     expect(recipeData).toMatchSnapshot()
     expect(recipeData).toHaveLength(9)
     expect(recipeData.map((recipe) => recipe.meal)).toEqual([
@@ -15,3 +15,13 @@ test('the recipe data is correct', () => {
         'Thai Gemüsecurry',
     ])
 })
+
+for (let i = 0; i < recipeData.length; i += 1) {
+    test(`recipe[${i}] should have properties (id, image, meal, ingredients, instructions)`, () => {
+        expect(recipeData[i]).toHaveProperty('id')
+        expect(recipeData[i]).toHaveProperty('image')
+        expect(recipeData[i]).toHaveProperty('meal')
+        expect(recipeData[i]).toHaveProperty('ingredients')
+        expect(recipeData[i]).toHaveProperty('instructions')
+    })
+}
