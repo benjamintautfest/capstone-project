61dcfd70791ec6fdbecab00cb2ea834295b00857
Benjamin Tautfest <b.tautfest@gmx.de>
2020-12-10 13:58:53 +0100

    Add Weekplan site

--- meal-planner/src/components/BookmarkPage.js
+++ meal-planner/src/components/BookmarkPage.js
@@ -1,13 +1,15 @@
 import styled from 'styled-components/macro'
 import meals from '../data/recipeData.json'
 
-export default function BookmarkPage() {
+export default function BookmarkPage({ weekdays }) {
     return (
         <DivStyled>
             <h1> Lieblingsessen </h1>
-            {meals.map(({ meal, image, id }) => (
+            {weekdays.map(({ meal, image, id, weekday }) => (
                 <div key={id} onClick={() => console.log('favourite')}>
-                    <h3>{meal}</h3>
+                    <h3>
+                        {weekday} + ' '{meal}
+                    </h3>
                     <img src={image} alt={meal} />
                 </div>
             ))}
